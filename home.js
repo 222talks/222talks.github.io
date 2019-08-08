c055c15370b48c032b270e3c30affe8f1f0f7637
222 Talks <51421950+222talks@users.noreply.github.com>
2019-08-08 02:07:44 -0400

    Groundwork for recent episodes

--- home.js
+++ home.js
@@ -1,2 +1,25 @@
 "use strict"
 
+function parseXML () {
+  var allCasts = []
+  var castList = xmlRequest.responseXML.getElementsByTagName("item");
+  for (var i = 0; i < castList.length; i++) {
+    var currentAttribs = castList[i].children;
+    var currentTitle = currentAttribs[0].innerHTML;
+    var currentDescription = currentAttribs[2].innerHTML;
+    var currentURL = currentAttribs[4].attributes[0].nodeValue;
+    var currentDate = currentAttribs[5].innerHTML;
+    allCasts.push([currentTitle, currentDescription, currentURL, currentDate]);
+  }
+  allCasts.sort(function(a, b) {
+    var c = new Date(a[4]);
+    var d = new Date(b[4]);
+    return c - d;
+  });
+  console.log(allCasts);
+}
+
+var xmlRequest = new XMLHttpRequest();
+xmlRequest.addEventListener("load", parseXML);
+xmlRequest.open("GET", "https://222talks.github.io/rss/latenighttalks.xml");
+xmlRequest.send();
