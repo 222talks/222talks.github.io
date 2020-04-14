54a6d6abdd9b88efe114b3f2c7cfa893ba57eccd
222 Talks <51421950+222talks@users.noreply.github.com>
2020-04-13 21:43:36 -0400

    Fix js

--- lnt.js
+++ lnt.js
@@ -6,6 +6,6 @@ var feedsToParse = 1;
 var xmlRequest1 = new XMLHttpRequest();
 xmlRequest1.open("GET", "https://222talks.github.io/rss/latenighttalks.xml");
 xmlRequest1.timeout = 4000;
-xmlRequest1.onload = function() {parseXML(xmlRequest, "Late Night Talks")};
+xmlRequest1.onload = function() {parseXML(xmlRequest1, "Late Night Talks")};
 xmlRequest1.ontimeout = function() {netError()};
 xmlRequest1.send();
