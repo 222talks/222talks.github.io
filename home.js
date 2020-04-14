e1ce8681f1410b8fe39fcea3e951554f37618d85
222 Talks <51421950+222talks@users.noreply.github.com>
2020-04-13 21:44:04 -0400

    Fix js

--- home.js
+++ home.js
@@ -6,13 +6,13 @@ var feedsToParse = 2;
 var xmlRequest1 = new XMLHttpRequest();
 xmlRequest1.open("GET", "https://222talks.github.io/rss/latenighttalks.xml");
 xmlRequest1.timeout = 4000;
-xmlRequest1.onload = function() {parseXML(xmlRequest, "Late Night Talks")};
+xmlRequest1.onload = function() {parseXML(xmlRequest1, "Late Night Talks")};
 xmlRequest1.ontimeout = function() {netError()};
 xmlRequest1.send();
 
 var xmlRequest2 = new XMLHttpRequest();
 xmlRequest2.open("GET", "https://222talks.github.io/rss/theJoeShow.xml");
 xmlRequest2.timeout = 4000;
-xmlRequest2.onload = function() {parseXML(xmlRequest, "The Joe Show")};
+xmlRequest2.onload = function() {parseXML(xmlRequest2, "The Joe Show")};
 xmlRequest2.ontimeout = function() {netError()};
 xmlRequest2.send();
